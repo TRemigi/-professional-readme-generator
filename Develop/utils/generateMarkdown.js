e311323c0da5e16b7631e55df532e6b8be8a2689
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-06-26 17:11:30 -0600

    finished rough of generateMarkdown.js

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -36,6 +36,8 @@ If your README is very long, add a table of contents to make it easy for users t
 * [Usage](#usage)
 * [Contributing](#contributing)
 * [License](#license)
+* [Testing](#testing)
+* [Questions](#questions)
 
 
 ## Installation
@@ -48,34 +50,26 @@ ${installation}
 ${usage}
 
 
+## License
+
+${license}
+
+
 ## Contributing
 
 ${contributing}
 
 
+## Testing
 
-## License
-
-${license}
-
----
-
-🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
-## Badges
-
-![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
-
-Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+${testing}
 
 
-## Contributing
+## Questions
 
-If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+GitHub Profile: ${questions.github}
 
-## Tests
-
-Go the extra mile and write tests for your application. Then provide examples on how to run them.
+Reach me via email at ${questions.email}
 
 `;
 };
