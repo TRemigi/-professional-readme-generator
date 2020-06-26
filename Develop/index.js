46c70758ebf786440b2f36fda3cc7cd9cd443814
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-06-26 14:46:30 -0600

    got init function running, added first two questions

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,55 @@
+// add generateMarkdown
+const generateMarkdown = require('./utils/generateMarkdown.js');
+// add inquirer
+const inquirer = require('inquirer');
+
 // array of questions for user
 const questions = [
-
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the name of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter the name of your project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please describe your project:',
+        validate: descInput => {
+            if (descInput) {
+                return true;
+            } else {
+                console.log('Please provide a description of your project');
+                return false;
+            }
+        }
+    }
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
 // function to initialize program
-function init() {
-
-}
+function init(questionsArr) {
+    const answerData = []
+    console.log(`
+    =================
+    Create New README
+    =================
+    `)
+    return inquirer.prompt(questionsArr)
+    .then(answers => {
+        answerData.push(answers);
+        console.log(answers)
+    });
+};
 
 // function call to initialize program
-init();
+init(questions);
