c055bd80d74e07ce1a0a3249b0ace6a8d9530b7a
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-06-26 17:05:35 -0600

    finished rough index.js and began generateMarkdown.js

--- Develop/index.js
+++ Develop/index.js
@@ -2,7 +2,8 @@
 const generateMarkdown = require('./utils/generateMarkdown.js');
 // add inquirer
 const inquirer = require('inquirer');
-
+// add fs
+const fs = require('fs');
 // array of questions for user
 const questions = [
     {
@@ -13,7 +14,7 @@ const questions = [
             if (titleInput) {
                 return true;
             } else {
-                console.log('Please enter the name of your project');
+                console.log('REQUIRED:');
                 return false;
             }
         }
@@ -21,12 +22,104 @@ const questions = [
     {
         type: 'input',
         name: 'description',
-        message: 'Please describe your project:',
+        message: 'Describe your project:',
         validate: descInput => {
             if (descInput) {
                 return true;
             } else {
-                console.log('Please provide a description of your project');
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide a description of how to install your project:',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage instructions and/or examples:',
+        validate: usageInput => {
+            if (usageInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license:',
+        choices: ['Apache 2.0', 'GNU GPLv3', 'MIT', 'ISC'],
+        validate: licenseInput => {
+            if (licenseInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide contribution guidelines:',
+        validate: contInput => {
+            if (contInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'testing',
+        message: 'Provide project testing instructions',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub Username?',
+        validate: githubInput => {
+            if (githubInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('REQUIRED:');
                 return false;
             }
         }
@@ -34,21 +127,37 @@ const questions = [
 ];
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, markDown) {
+    
+    fs.writeFile(fileName, markDown, function(err) {
+        if (err) {
+          return console.log(err);
+        }
+  
+        console.log('README created!');
+      });
+};
 
 // function to initialize program
 function init(questionsArr) {
-    const answerData = []
+    // const answerData = [];
     console.log(`
     =================
     Create New README
     =================
-    `)
+    `);
     return inquirer.prompt(questionsArr)
     .then(answers => {
-        answerData.push(answers);
-        console.log(answers)
-    });
+        return generateMarkdown(answers)
+    })
+    .then(markDown => {
+        const fileName = markDown
+        .toLowerCase()
+        .split('# ')[1]
+        .split('\n')[0]
+        + '-README.md';
+        writeToFile(fileName, markDown)
+    })
 };
 
 // function call to initialize program
